382ae01b324234e593f88a8ca40491fa6ee015b7
LBJ-development <ludwig@jablonski.info>
2015-09-17 17:30:08 -0400

    Updating the alignment of the fields

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,8 @@
 
 angular.module('RFIapp.config', [])
   .constant('RFIConfig', {
-    contextPath:'http://172.20.2.13:8080/rfi'
+    //contextPath:'http://172.20.2.13:8080/rfi'
+    contextPath:'http://172.20.2.153:8080/rfi'
+  
 });
 
